df82bfb30e74424a64f43427666e70c74217583a
bex3 <rebecca.m.bearden@gmail.com>
2017-02-03 16:27:02 -0500

    starting on runner

---
+++ src/Runner.java
@@ -0,0 +1,11 @@
+/**
+ * Created by bearden-tellez on 2/3/17.
+ */
+public class Runner {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Guess the Common Denominator" + "\n" + "\nFor example Water, Juice, and Coffee  \nThe answer would be drinks");
+        System.out.println("If you would like to exit at any time press 0");
+
+
+    }
+}
