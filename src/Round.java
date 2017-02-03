35ea5438eb4e85adfb71439dd87ea51bc26a0e49
bex3 <rebecca.m.bearden@gmail.com>
2017-02-03 15:23:39 -0500

    trying to make it more efficient

---
+++ src/Round.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+
+/**
+ * Created by bearden-tellez on 2/3/17.
+ */
+public class Round {
+    //round composed of al categories, hint & answer
+
+    ArrayList <String> roundOne = new ArrayList<>();
+    ArrayList <String> roundTwo = new ArrayList<>();
+    ArrayList <String> roundThree = new ArrayList<>();
+    ArrayList <String> roundFour= new ArrayList<>();
+    ArrayList <String> roundFive = new ArrayList<>();
+    ArrayList <String> roundSix = new ArrayList<>();
+    ArrayList <String> roundSeven = new ArrayList<>();
+    ArrayList <String> roundEight = new ArrayList<>();
+    ArrayList <String> roundNine = new ArrayList<>();
+    ArrayList <String> roundTen = new ArrayList<>();
+    ArrayList <String> roundEleven = new ArrayList<>();
+    ArrayList <String> roundTwelve= new ArrayList<>();
+    ArrayList <String> roundThirteen = new ArrayList<>();
+    ArrayList <String> roundFourteen = new ArrayList<>();
+    ArrayList <String> roundFifteen = new ArrayList<>();
+    ArrayList <String> roundSixteen = new ArrayList<>();
+    ArrayList <String> roundSeventeen = new ArrayList<>();
+    ArrayList <String> roundEighteen = new ArrayList<>();
+    ArrayList <String> roundNineteen = new ArrayList<>();
+    ArrayList <String> roundTwenty = new ArrayList<>();
+
+    public void add(int index, String words){
+        roundOne.add(0, "Red");
+        roundOne.add(1,"Green");
+        roundOne.add(2, "Yellow");
+    }
+}
