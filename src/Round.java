1fa0baeb50c2abf2d727b0152b01dc2f5a02800c
bex3 <rebecca.m.bearden@gmail.com>
2017-02-03 19:19:52 -0500

    rounds in runner

--- src/Round.java
+++ src/Round.java
@@ -5,161 +5,180 @@ import java.util.ArrayList;
  */
 public class Round {
     //round composed of al categories, hint & answer
+    String answer;
+    String secondAnswer;
+    String hint;
+    String word1;
+    String word2;
+    String word3;
+    String word4;
+    ArrayList<String> wordsForRound = new ArrayList<>();
 
-    String hintOne = "You use these to paint or draw with";
-    String hintTwo = "You count with these";
-    String hintThree = "Saturday morning ________";
-    String hintFour = "A golden TV show";
-    String hintFive = "Relax with one of these";
-    String hintSix = "Modes of transportation";
-    String hintSeven = "Way down yonder on the Chattahoochee";
-    String hintEight = "Think Gambling & Vegas";
-    String hintNine = "This game is written in one of these";
-    String hintTen = "Bang Bang";
-    String hintEleven = "Man's best friend";
-    String hintTwelve = "You can climb these";
-    String hintThirteen = "Pay attention to your watch when traveling across these";
-    String hintFourteen = "Historical and cultural region in Northern Europe";
-    String hintFifteen = "Write it down";
-    String hintSixteen = "Play for a cool tune";
-    String hintSeventeen = "Listen Carefully";
-    String hintEighteen = "The fuel of programmers";
-    String hintNineteen = "Not iron or steel";
-    String hintTwenty = "Neil Tyson Degrasse";
+//    String hintOne = "You use these to paint or draw with";
+//    String hintTwo = "You count with these";
+//    String hintThree = "Saturday morning ________";
+//    String hintFour = "A golden TV show";
+//    String hintFive = "Relax with one of these";
+//    String hintSix = "Modes of transportation";
+//    String hintSeven = "Way down yonder on the Chattahoochee";
+//    String hintEight = "Think Gambling & Vegas";
+//    String hintNine = "This game is written in one of these";
+//    String hintTen = "Bang Bang";
+//    String hintEleven = "Man's best friend";
+//    String hintTwelve = "You can climb these";
+//    String hintThirteen = "Pay attention to your watch when traveling across these";
+//    String hintFourteen = "Historical and cultural region in Northern Europe";
+//    String hintFifteen = "Write it down";
+//    String hintSixteen = "Play for a cool tune";
+//    String hintSeventeen = "Listen Carefully";
+//    String hintEighteen = "The fuel of programmers";
+//    String hintNineteen = "Not iron or steel";
+//    String hintTwenty = "Neil Tyson Degrasse";
+//
+//    String answerOne = "colors";
+//    String answerTwo = "numbers";
+//    String answerThree = "cartoons";
+//    String answerFour = "golden girls";
+//    String answerFive = "beer";
+//    String answerSix = "horses";
+//    String answerSeven = "rivers";
+//    String answerEight = "poker hands";
+//    String answerNine = "programming languages";
+//    String answerTen = "gun brands";
+//    String answerEleven = "dog breeds";
+//    String answerTwelve = "trees";
+//    String answerThirteen = "time zones";
+//    String answerFourteen = "Scandinavian countries";
+//    String answerFifteen = "paper types";
+//    String answerSixteen = "musical instruments";
+//    String answerSeventeen = "ear bones";
+//    String answerEighteen = "coffee";
+//    String answerNineteen = "non-ferrous metals";
+//    String answerTwenty = "moons of Neptune";
+//
+//    ArrayList <String> roundOne = new ArrayList<>();
+//    ArrayList <String> roundTwo = new ArrayList<>();
+//    ArrayList <String> roundThree = new ArrayList<>();
+//    ArrayList <String> roundFour= new ArrayList<>();
+//    ArrayList <String> roundFive = new ArrayList<>();
+//    ArrayList <String> roundSix = new ArrayList<>();
+//    ArrayList <String> roundSeven = new ArrayList<>();
+//    ArrayList <String> roundEight = new ArrayList<>();
+//    ArrayList <String> roundNine = new ArrayList<>();
+//    ArrayList <String> roundTen = new ArrayList<>();
+//    ArrayList <String> roundEleven = new ArrayList<>();
+//    ArrayList <String> roundTwelve= new ArrayList<>();
+//    ArrayList <String> roundThirteen = new ArrayList<>();
+//    ArrayList <String> roundFourteen = new ArrayList<>();
+//    ArrayList <String> roundFifteen = new ArrayList<>();
+//    ArrayList <String> roundSixteen = new ArrayList<>();
+//    ArrayList <String> roundSeventeen = new ArrayList<>();
+//    ArrayList <String> roundEighteen = new ArrayList<>();
+//    ArrayList <String> roundNineteen = new ArrayList<>();
+//    ArrayList <String> roundTwenty = new ArrayList<>();
+//
+//
+//    public void add(int index, String words){
+//        roundOne.add(0, "Red");
+//        roundOne.add(1,"Green");
+//        roundOne.add(2, "Yellow");
+//
+//        roundTwo.add(0, "Primes");
+//        roundTwo.add(1, "Evens");
+//        roundTwo.add(2, "Odds");
+//
+//        roundThree.add(0, "Spongebob");
+//        roundThree.add(1, "Tom & Jerry");
+//        roundThree.add(2, "Courage The Cowardly Dog");
+//
+//        roundFour.add(0, "Blanche");
+//        roundFour.add(1, "Rose");
+//        roundFour.add(2, "Dorothy");
+//        roundFour.add(3, "Sophia");
+//
+//        roundFive.add(0, "Hops");
+//        roundFive.add(1, "Yeast");
+//        roundFive.add(2, "Water");
+//        roundFive.add(3, "Grain");
+//
+//        roundSix.add(0, "Bronco");
+//        roundSix.add(1, "Mustang");
+//        roundSix.add(2, "Brumby");
+//
+//        roundSeven.add(0, "Colorado");
+//        roundSeven.add(1, "Yellow");
+//        roundSeven.add(2, "Amazon");
+//
+//        roundEight.add(0, "Flush");
+//        roundEight.add(1,"Straight");
+//        roundEight.add(2, "Full House");
+//
+//        roundNine.add(0, "Java");
+//        roundNine.add(1, "Ruby");
+//        roundNine.add(2, "C");
+//
+//        roundTen.add(0, "Winchester");
+//        roundTen.add(1, "Remington");
+//        roundTen.add(2, "Colt");
+//
+//        roundEleven.add(0, "Chinook");
+//        roundEleven.add(1, "Azawakh");
+//        roundEleven.add(2, "Mudi");
+//
+//        roundTwelve.add(0, "Larch");
+//        roundTwelve.add(1, "Linden");
+//        roundTwelve.add(2, "Orange");
+//
+//        roundThirteen.add(0, "Pacific");
+//        roundThirteen.add(1, "Mountain");
+//        roundThirteen.add(2, "Central");
+//
+//        roundFourteen.add(0, "Denmark");
+//        roundFourteen.add(1, "Norway");
+//        roundFourteen.add(2, "Sweden");
+//
+//        roundFifteen.add(0, "Glossy");
+//        roundFifteen.add(1, "Plain");
+//        roundFifteen.add(2, "Heavy");
+//
+//        roundSixteen.add(0, "Cajon");
+//        roundSixteen.add(1, "Lur");
+//        roundSixteen.add(2, "Crwth");
+//
+//        roundSeventeen.add(0, "Hammer");
+//        roundSeventeen.add(1, "Anvil");
+//        roundSeventeen.add(2, "Stirrup");
+//
+//        roundEighteen.add(0, "Cortadita");
+//        roundEighteen.add(1, "Breve");
+//        roundEighteen.add(2,  "Vienna");
+//
+//        roundNineteen.add(0, "Copper");
+//        roundNineteen.add(1, "Brass");
+//        roundNineteen.add(2, "Zinc");
+//
+//        roundTwenty.add(0, "Despina");
+//        roundTwenty.add(1, "Galatea");
+//        roundTwenty.add(2, "Larissa");
+//    }
+//
+//    public Round() {
+//    }
 
-    String answerOne = "colors";
-    String answerTwo = "numbers";
-    String answerThree = "cartoons";
-    String answerFour = "golden girls";
-    String answerFive = "beer";
-    String answerSix = "horses";
-    String answerSeven = "rivers";
-    String answerEight = "poker hands";
-    String answerNine = "programming languages";
-    String answerTen = "gun brands";
-    String answerEleven = "dog breeds";
-    String answerTwelve = "trees";
-    String answerThirteen = "time zones";
-    String answerFourteen = "Scandinavian countries";
-    String answerFifteen = "paper types";
-    String answerSixteen = "musical instruments";
-    String answerSeventeen = "ear bones";
-    String answerEighteen = "coffee";
-    String answerNineteen = "non-ferrous metals";
-    String answerTwenty = "moons of Neptune";
-
-    ArrayList <String> roundOne = new ArrayList<>();
-    ArrayList <String> roundTwo = new ArrayList<>();
-    ArrayList <String> roundThree = new ArrayList<>();
-    ArrayList <String> roundFour= new ArrayList<>();
-    ArrayList <String> roundFive = new ArrayList<>();
-    ArrayList <String> roundSix = new ArrayList<>();
-    ArrayList <String> roundSeven = new ArrayList<>();
-    ArrayList <String> roundEight = new ArrayList<>();
-    ArrayList <String> roundNine = new ArrayList<>();
-    ArrayList <String> roundTen = new ArrayList<>();
-    ArrayList <String> roundEleven = new ArrayList<>();
-    ArrayList <String> roundTwelve= new ArrayList<>();
-    ArrayList <String> roundThirteen = new ArrayList<>();
-    ArrayList <String> roundFourteen = new ArrayList<>();
-    ArrayList <String> roundFifteen = new ArrayList<>();
-    ArrayList <String> roundSixteen = new ArrayList<>();
-    ArrayList <String> roundSeventeen = new ArrayList<>();
-    ArrayList <String> roundEighteen = new ArrayList<>();
-    ArrayList <String> roundNineteen = new ArrayList<>();
-    ArrayList <String> roundTwenty = new ArrayList<>();
-
-
-    public void add(int index, String words){
-        roundOne.add(0, "Red");
-        roundOne.add(1,"Green");
-        roundOne.add(2, "Yellow");
-
-        roundTwo.add(0, "Primes");
-        roundTwo.add(1, "Evens");
-        roundTwo.add(2, "Odds");
-
-        roundThree.add(0, "Spongebob");
-        roundThree.add(1, "Tom & Jerry");
-        roundThree.add(2, "Courage The Cowardly Dog");
-
-        roundFour.add(0, "Blanche");
-        roundFour.add(1, "Rose");
-        roundFour.add(2, "Dorothy");
-        roundFour.add(3, "Sophia");
-
-        roundFive.add(0, "Hops");
-        roundFive.add(1, "Yeast");
-        roundFive.add(2, "Water");
-        roundFive.add(3, "Grain");
-
-        roundSix.add(0, "Bronco");
-        roundSix.add(1, "Mustang");
-        roundSix.add(2, "Brumby");
-
-        roundSeven.add(0, "Colorado");
-        roundSeven.add(1, "Yellow");
-        roundSeven.add(2, "Amazon");
-
-        roundEight.add(0, "Flush");
-        roundEight.add(1,"Straight");
-        roundEight.add(2, "Full House");
-
-        roundNine.add(0, "Java");
-        roundNine.add(1, "Ruby");
-        roundNine.add(2, "C");
-
-        roundTen.add(0, "Winchester");
-        roundTen.add(1, "Remington");
-        roundTen.add(2, "Colt");
-
-        roundEleven.add(0, "Chinook");
-        roundEleven.add(1, "Azawakh");
-        roundEleven.add(2, "Mudi");
-
-        roundTwelve.add(0, "Larch");
-        roundTwelve.add(1, "Linden");
-        roundTwelve.add(2, "Orange");
-
-        roundThirteen.add(0, "Pacific");
-        roundThirteen.add(1, "Mountain");
-        roundThirteen.add(2, "Central");
-
-        roundFourteen.add(0, "Denmark");
-        roundFourteen.add(1, "Norway");
-        roundFourteen.add(2, "Sweden");
-
-        roundFifteen.add(0, "Glossy");
-        roundFifteen.add(1, "Plain");
-        roundFifteen.add(2, "Heavy");
-
-        roundSixteen.add(0, "Cajon");
-        roundSixteen.add(1, "Lur");
-        roundSixteen.add(2, "Crwth");
-
-        roundSeventeen.add(0, "Hammer");
-        roundSeventeen.add(1, "Anvil");
-        roundSeventeen.add(2, "Stirrup");
-
-        roundEighteen.add(0, "Cortadita");
-        roundEighteen.add(1, "Breve");
-        roundEighteen.add(2,  "Vienna");
-
-        roundNineteen.add(0, "Copper");
-        roundNineteen.add(1, "Brass");
-        roundNineteen.add(2, "Zinc");
-
-        roundTwenty.add(0, "Despina");
-        roundTwenty.add(1, "Galatea");
-        roundTwenty.add(2, "Larissa");
+    public Round(String answer, String hint, String word1, String word2, String word3){
+        this.answer= answer;
+        this.hint = hint;
+        this.wordsForRound.add(word1);
+        this.wordsForRound.add(word2);
+        this.wordsForRound.add(word3);
     }
 
-    public Round() {
-    }
-
-    public Round(String answer, String hint, ArrayList<String> roundOne){
-        this.answerOne= answer;
-        this.hintOne = hint;
-        this.roundOne= roundOne;
+    public Round(String answer, String hint, String word1, String word2, String word3, String word4){
+        this.answer= answer;
+        this.hint = hint;
+        this.wordsForRound.add(word1);
+        this.wordsForRound.add(word2);
+        this.wordsForRound.add(word3);
+        this.wordsForRound.add(word4);
     }
 }
--- src/Runner.java
+++ src/Runner.java
@@ -7,7 +7,26 @@ public class Runner {
     Scanner inputScanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-
+        Round roundOne = new Round("colors", "You use these to paint or draw with", "Red", "Green", "Yellow");
+        Round roundTwo= new Round("Numbers", "You count with these", "Primes", "Evens", "Odds");
+        Round roundThree = new Round("cartoons", "Saturday morning ________", "Spongebob", "Tom & Jerry", "Courage The Cowardly Dog");
+        Round roundFour = new Round("Golden Girls", "A golden TV show", "Blanche", "Rose", "Dorothy", "Sophia");
+        Round roundFive = new Round("beer", "Relax with one of these", "Hops", "Yeast", "Water", "Grain") ;
+        Round roundSix = new Round("Cars named for horses", "Modes of transportation named after predecessors", "Bronco", "Mustang", "Brumby");
+        Round roundSeven = new Round("River", "Way down yonder on the Chattahoochee","Colorado", "Yellow", "Amazon");
+        Round roundEight = new Round("Poker Hands", "Think Gambling & Vegas", "Flush", "Straight", "Full House");
+        Round roundNine = new Round("Programming languages","This game is written in one of these", "Java", "Ruby", "C");
+        Round roundTen = new Round("gun brands", "bang bang", "Remington", "Colt", "Winchester");
+        Round roundEleven = new Round("dog breeds", "Man's best friend", "Chinook", "Azawakh", "Mudi");
+        Round roundTwelve = new Round("trees", "You can climb these", "Larch", "Linden", "Orange" );
+        Round roundThirteen = new Round("time zones", "Pay attention to your watch when you cross these", "Pacific", "Mountain", "Central");
+        Round roundFourteen = new Round("Scandinavian countries", "Historical and cultural region in Northern Europe...or Vikings", "Denmark", "Norway", "Sweden");
+        Round roundFifteen = new Round("paper types", "Write it down", "Glossy", "Plain", "Heavy");
+        Round roundSixteen = new Round("musical instruments", "Play a cool tune", "Lur", "Crwth", "Cajon");
+        Round roundSeventeen = new Round("ear bones", "Listen carefully", "Hammer", "Anvil", "Stirrup");
+        Round roundEighteen = new Round("coffee", "Fuel for programmers", "Cortadita", "Vienna", "Breve");
+        Round roundNineteen = new Round("non-ferrous metals", "Not iron or steel", "Zinc", "Brass", "Copper");
+        Round roundTwenty = new Round("moons of Neptune", "Neil Tyson Degrasse", "Despina", "Galatea", "Larissa");
     }
 
     public void gameMenu() {
@@ -15,12 +34,8 @@ public class Runner {
         System.out.println("If you get an answer wrong, a hint will be displayed on the screen and you will be returned to the level that was incomplete. Also, most answers are in plural form");
         System.out.println("If you would like to exit at any time press 0");
 
-        Round theRound = new Round();
-
         String userInput = inputScanner.nextLine();
-//        if(userInput.equalsIgnoreCase()){
-//
-//        }
+        
 
 
     }
