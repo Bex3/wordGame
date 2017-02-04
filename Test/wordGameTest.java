f311fb2c1c62c01c6084230c3d6a08b51203181b
bex3 <rebecca.m.bearden@gmail.com>
2017-02-04 10:39:00 -0500

    removed import that was a misstype

--- Test/wordGameTest.java
+++ Test/wordGameTest.java
@@ -4,7 +4,6 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertFalse;
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 /**
